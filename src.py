9b1c6c8dc4fae62c9dba6e40bca6ff141b9b71cc
Kyle <n.kyle.ma@gmail.com>
2016-11-09 06:37:01 -0800

    Bug Fixes
    StatBot doesn't get caught in a redundant  loop

--- src.py
+++ src.py
@@ -21,19 +21,16 @@ class StatBot(Browser):
 
     def __init__(self):
         Browser.__init__(self, 'http://www.saltybet.com/')
-        self.wins = 0
-        self.losses = 0
-        self.balance = 0
-        self.win_streak = 0
-        self.loss_streak = 0
 
     def get_crowd_stats(self):
         """Scrapes site to find who the crowd favorite of the voters are"""
         red_team = []
         # Wait until votes are tallied
         while len(red_team) < 2:
+            self.soup = BeautifulSoup(self.session.body(), 'lxml')
             red_team = self.soup.find('span', class_='redtext').text.split('|')
             blue_team = self.soup.find('span', class_='bluetext').text.split('|')
+            time.sleep(1)
 
         self.time_start = time.time()
         red_team.append('Red')
@@ -46,43 +43,63 @@ class StatBot(Browser):
         while 'wins' not in outcome:
             self.soup = BeautifulSoup(self.session.body(), 'lxml')
             outcome = self.soup.find('span', id='betstatus').text
+            time.sleep(1)
 
         self.time_end = time.time()
         self.winner = outcome.split()[-1][:-1]
 
     def record_data(self, name, win, time_elapsed, votes):
         """Writes data to database session"""
+        name = name.strip()
         query = session.query(Fighter).filter_by(name=name).first()
         if query:
             fighter = session.query(Fighter).filter_by(name=name).first()
         else:
-            fighter = Fighter(name=name)
+            fighter = Fighter(name=name, wins=0, losses=0)
             session.add(fighter)
             session.commit()
             fighter = session.query(Fighter).filter_by(name=name).first()
 
-        fighter.average_percent_of_votes = int(fighter.average_number_of_votes) + int(votes)
-        if win:
-            fighter.wins = int(fighter.wins) + 1
-            fighter.average_win_time = (int(fighter.average_win_time) + time_elapsed) / 2
+        if int(fighter.average_percent_of_votes):
+            fighter.average_percent_of_votes = (int(fighter.average_percent_of_votes) + int(votes)) / 2
         else:
-            fighter.losses += 1
-            fighter.average_loss_time += (int(fighter.average_loss_time) + time_elapsed) / 2
+            fighter.average_percent_of_votes = (int(fighter.average_percent_of_votes) + int(votes))
 
-        session.add(fighter)
+        if win:
+            fighter.wins += 1
+            if int(fighter.average_win_time):
+                fighter.average_win_time = (int(fighter.average_win_time) + time_elapsed) / 2
+            else:
+                fighter.average_win_time = (int(fighter.average_win_time) + time_elapsed)
+        else:
+            fighter.losses +=1
+            if int(fighter.average_loss_time):
+                fighter.average_loss_time += (int(fighter.average_loss_time) + time_elapsed) / 2
+            else:
+                fighter.average_loss_time += (int(fighter.average_loss_time) + time_elapsed)
+
         session.commit()
 
-    def run_recording_session(self):
+    def wait_until_next_fight(self):
+        """Wait until next round of fight"""
+        outcome = 'random string'
+        while 'OPEN!' not in outcome:
+            self.soup = BeautifulSoup(self.session.body(), 'lxml')
+            outcome = self.soup.find('span', id='betstatus').text
+            time.sleep(1)
 
+    def run_recording_session(self):
+        i = 0
         while True:
             self.get_crowd_stats()
             self.get_winner()
             elapsed_time = self.time_end - self.time_start
-            total_votes = self.red_team[0] + self.blue_team[1]
+            total_votes = int(self.red_team[0]) + int(self.blue_team[1])
             self.record_data(name=self.red_team[1], win=self.winner == self.red_team[-1],
-                             time_elapsed=elapsed_time, votes=self.red_team[0] / total_votes)
+                             time_elapsed=elapsed_time, votes=float(self.red_team[0]) / total_votes * 100)
             self.record_data(name=self.blue_team[0], win=self.winner == self.blue_team[-1],
-                             time_elapsed=elapsed_time, votes=self.blue_team[1] / total_votes)
+                             time_elapsed=elapsed_time, votes=float(self.blue_team[1]) / total_votes * 100)
+            self.wait_until_next_fight()
 
 if __name__ == '__main__':
 
